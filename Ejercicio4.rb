7b78c9b9b1f9ef224e06ee6c914d2477faa1ec39
Zamiz <zamiztobar@gmail.com>
2018-10-23 20:26:39 -0300

    *Ejercicio 4+método ladrar

--- Ejercicio4.rb
+++ Ejercicio4.rb
@@ -5,6 +5,9 @@ class Dog
     @raza = propiedades[:raza]
     @color = propiedades[:color]
   end
+  def ladrar
+    puts "#{nombre} está ladrando!"
+  end
 end
 
 # propiedades = {nombre: 'Beethoven', raza: 'San Bernardo', color: 'Café'}
@@ -13,3 +16,4 @@ propiedades = {nombre: 'Dobby', raza: 'Elfo', color: 'Café Claro'}
 # dog_dobby = Dog.new('Dobby','Elfo','Piel')
 dog_dobby = Dog.new(propiedades)
 puts dog_dobby.nombre
+dog_dobby.ladrar
