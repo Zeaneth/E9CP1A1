1973d65b823da53fdab8c985e86021c6779cfd3a
Zamiz <zamiztobar@gmail.com>
2018-10-23 21:06:08 -0300

    +Ejercicio 5

--- Ejercicio5.rb
+++ Ejercicio5.rb
@@ -1,1 +1,36 @@
-class Morseable  def initialize(number)    @number = number  end  def generate_hash(number)    # Esto es una aberración y debe ser refactorizado!    h = '-----' if number == 0    h = '.----' if number == 1    h = '..---' if number == 2    h = '...--' if number == 3    h = '....-' if number == 4    h = '.....' if number == 5    h = '-....' if number == 6    h = '--...' if number == 7    h = '---..' if number == 8    h = '----.' if number == 9  end  def to_morse    self.generate_hash(@number)  endendm = Morseable.new(3)print m.to_morse
+class Morseable
+  def initialize(number)
+    @number = number
+  end
+  def generate_hash(index)
+    # Esto es una aberración y debe ser refactorizado!
+    # case number
+    # when 1
+    #   puts '-----'
+    # when 2
+    #   puts '.----'
+    # when 3
+    #   puts '..---'
+    # when 4
+    #   puts '...--'
+    # when 5
+    #   puts '....-'
+    # when 6
+    #   puts '-....'
+    # when 7
+    #   puts '--...'
+    # when 8
+    #   puts '---..'
+    # when 9
+    #   puts '----.'
+    # end
+    hash = [nil,'-----','.----','..---','...--','....-','-....','--...','---..','----.']
+    hash[index]
+  end
+  def to_morse
+    self.generate_hash(@number)
+  end
+end
+
+m = Morseable.new(3)
+print m.to_morse
